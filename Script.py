4f68a72f459f9b62dd9b1db2f884acd4960bae26
Juanes2001 <jrodriguezoc@unal.edu.co>
2025-07-16 15:43:56 -0500

    c

--- Script.py
+++ Script.py
@@ -8,23 +8,23 @@ data = []
 if __name__ == '__main__':
 
 
-    arr = np.array([[1,2], [3,4]])
-    print(arr[:][0])
+    # arr = np.array([[1,2], [3,4]])
+    # print(arr[:][0])
 
 
-    # serial_ = fn.create_ser('COM9', 9600, timeout=1, write_timeout=1)
-    # fn.openSer(ser = serial_)
-    # try:
-    #     while True:
-    #         command = input("Ingrese comando: ")
-    #         result = fn.process_command(serial_,command)
-    #         if result is not None:
-    #             data.append(result)
-    #         print(data)
-    #
-    #
-    # except KeyboardInterrupt:
-    #     # fn.close_ser(serial_)
-    #     print("\nFin de la ejecución\n")
+    serial_ = fn.create_ser('COM9', 9600, timeout=1, write_timeout=1)
+    fn.openSer(ser = serial_)
+    try:
+        while True:
+            command = input("Ingrese comando: ")
+            result = fn.process_command(serial_,command)
+            if result is not None:
+                data.append(result)
+            print(data)
+
+
+    except KeyboardInterrupt:
+        fn.close_ser(serial_)
+        print("\nFin de la ejecución\n")
 
 
